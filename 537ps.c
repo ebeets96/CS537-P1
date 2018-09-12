988f9480cd0c4c58b570c6a1f8899b46d2cb045c
ebeets96 <eric@ericbeets.com>
2018-09-12 15:25:41 -0500

    Add modularity with ps function

--- 537ps.c
+++ 537ps.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <stdbool.h> 
 #include "537ps.h"
 
 int main(int argc, char* argv[]) {
-	
+	// Handle passed arguments
+
+	// Call ps function with all true values
+	// Replace when argument passing is accomplished
+	ps(-1, true, true, true, true, true);
 	return 0;
+}
+
+/**
+ * pid is process ID to display and equals -1 to show all processes
+ * state represents -s flag
+ * userTime represents -U flag
+ * sysTime represents -S flag
+ * vMem represents -v flag
+ * comLine represents -c flag
+**/
+
+void ps(int pid, bool state, bool userTime, bool sysTime, bool vMem, bool comLine) {
+
 }
