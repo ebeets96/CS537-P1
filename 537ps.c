b36f866be3b607dbee318d2c640a9d1ddbfb1f6b
Eric Beets <eric@ericbeets.com>
2018-09-13 15:09:28 -0500

    Readdir

--- 537ps.c
+++ 537ps.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <dirent.h>
+#include <sys/types.h>
 #include "537ps.h"
 
 int main(int argc, char* argv[]) {
@@ -16,15 +18,15 @@ int main(int argc, char* argv[]) {
 
 	// Handle passed arguments
 	int opt;
-	while ((opt = getopt(argc, argv, "p:sUSvc")) != -1) {
-		printf("%c : ", opt);
-		printf("%s\n", optarg);
-	}
+	// while ((opt = getopt(argc, argv, "p:sUSvc")) != -1) {
+	// 	printf("%c : ", opt);
+	// 	printf("%s\n", optarg);
+	// }
 
 	// Call ps function with all true values
 	// Replace when argument passing is accomplished
 
-	//printAll(true, true, true, true, true);
+	printAll(true, true, true, true, true);
 	// printOne(atoi(argv[1]), true, true, true, true, true); //Dummy pid for testing
 
 	return 0;
@@ -32,7 +34,18 @@ int main(int argc, char* argv[]) {
 
 void printAll(bool state, bool userTime, bool sysTime, bool vMem, bool comLine) {
 	// Read folders in /proc directory
-
+	printf("Called");
+	DIR *dir;
+	struct dirent *entry;
+	if ((dir = opendir("/proc")) == NULL) {
+		//Error handling
+		printf("errors");
+	} else {
+		while ((entry = readdir(dir)) != NULL) {
+			printf("  %s\n", entry->d_name);
+		}
+		closedir(dir);
+	}
 	// Call printOne on each process
 }
 
