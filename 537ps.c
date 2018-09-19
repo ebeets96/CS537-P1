93e3047bbb195044aca88393f3f75bd5f9a65d7e
Eric Beets <ebeets@wisc.edu>
2018-09-18 20:26:59 -0500

    Revert "Checked to see if user id matched process user id"
    This reverts commit 7d64fefb912d90b79961bc1c75d911720ec79991.

--- 537ps.c
+++ 537ps.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-#include <unistd.h>
-#include <dirent.h>
-#include <sys/types.h>
 #include "537ps.h"
 
-const int bufSize = 100;
-
 int main(int argc, char* argv[]) {
+	// // For debugging
+	// if(argc < 2)
+	// 	return -1;
 	
-	char* pid = "";
+	int pid = -1;
 	bool s = false;
 	bool U = true;
 	bool S = false;
@@ -19,12 +18,13 @@ int main(int argc, char* argv[]) {
 	bool c = true;
 	bool validEntry = true;
 
+	// Handle passed arguments
 	char opt;
 	while ((opt = getopt(argc, argv, "p:s::U::S::v::c::")) != -1) {
 		switch (opt) {
 			case 'p':
 				if (optarg != NULL && atoi(optarg) != 0) {
-					pid = optarg;
+					pid = atoi(optarg);
 				} else {
 					printf("Must provide a PID integer\n");
 					validEntry = false;
@@ -87,36 +87,32 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
-	if (pid == "") {
-		printAll(s, U, S, v, c);
-	} else {
-		printOne(pid, s, U, S, v, c);
-	}
+	// For testing 
+	printf("pid: %d\n", pid);
+	printf("s flag: %s\n", s ? "true" : "false");
+	printf("U flag: %s\n", U ? "true" : "false");
+	printf("S flag: %s\n", S ? "true" : "false");
+	printf("v flag: %s\n", v ? "true" : "false");
+	printf("c flag: %s\n", c ? "true" : "false");
+	//
+	
+	// Call ps function with all true values
+	// Replace when argument passing is accomplished
+
+	//printAll(true, true, true, true, true);
+	// printOne(atoi(argv[1]), true, true, true, true, true); //Dummy pid for testing
 
 	return 0;
 }
 
 void printAll(bool state, bool userTime, bool sysTime, bool vMem, bool comLine) {
 	// Read folders in /proc directory
-	DIR *dir;
-	struct dirent *entry;
-	if ((dir = opendir("/proc")) == NULL) {
-		//Error handling
-		printf("errors");
-	} else {
-		while ((entry = readdir(dir)) != NULL) {
-			int folderValue = atoi(entry->d_name);
-			if(folderValue != 0 || entry->d_name[0] == '0') {
-				//Folder is numeric
-				printOne(entry->d_name, state, userTime, sysTime, vMem, comLine);
-			}
-		}
-		closedir(dir);
-	}
+
+	// Call printOne on each process
 }
 
 /**
- * pid is process ID to display
+ * pid is process ID to display and equals -1 to show all processes
  * state represents -s flag
  * userTime represents -U flag
  * sysTime represents -S flag
@@ -124,113 +120,6 @@ void printAll(bool state, bool userTime, bool sysTime, bool vMem, bool comLine) 
  * comLine represents -c flag
 **/
 
-void printOne(char* pid, bool state, bool userTime, bool sysTime, bool vMem, bool comLine) {
-	// Load files required for ps command
-	char procRoot[bufSize];
-        strcpy(procRoot, "/proc/");
-	strcat(procRoot, pid);
-
-	char statusFilename[bufSize];
-	strcpy(statusFilename, procRoot);
-	strcat(statusFilename, "/status");
-
-	char statFilename[bufSize];
-	strcpy(statFilename, procRoot);
-	strcat(statFilename, "/stat");
-
-	char statmFilename[bufSize];
-	strcpy(statmFilename, procRoot);
-	strcat(statmFilename, "/statm");
-
-	char cmdFilename[bufSize];
-	strcpy(cmdFilename, procRoot);
-	strcat(cmdFilename, "/cmdline");
-
-	// Create variables for process data
-	char stat_state;
-	int stat_pid;
-	unsigned long int utime;
-	unsigned long int stime;
-	int vMemVal;
-	char command[bufSize];
-
-	// Check status file
-	FILE *statusFile = fopen(statusFilename, "r");
-	if(statusFile == NULL) {
-		return;
-	} else {
-		// Get user id
-		int uid = getuid();
-		char statusLine[bufSize];
-		// Get to the correct line in the status file
-		for (int i = 0; i < 8; i++) {
-			if (fgets(statusLine, bufSize, statusFile) == NULL) {
-				return;
-			}
-		}
-		int process_uid;
-		// Get process uid
-		fscanf(statusFile, "%*s %d", &process_uid);
-		// Check that uid matches process uid
-		if (uid != process_uid) {
-			return;
-		}
-	}
-	fclose(statusFile);		
-
-	// Check stat file
-	FILE *statFile = fopen(statFilename, "r");
-	if(statFile == NULL) {
-		return;
-	} else {
-		// Load data from stat file
-		fscanf(statFile, "%d %*s %c %*d %*d %*d %*d %*d %*u %*u %*u %*u %*u %lu %lu ", &stat_pid, &stat_state, &utime, &stime);
-	}
-	fclose(statFile);
-
-	// Check statm file
-	FILE *statmFile = fopen(statmFilename, "r");
-	if(statmFile == NULL) {
-		return;
-	} else {
-		// Load data from stat file
-		fscanf(statmFile, "%d", &vMemVal);
-	}
-	fclose(statmFile);
-
-	// Check cmdLine file
-	FILE *cmdFile = fopen(cmdFilename, "r");
-	if(cmdFile == NULL) {
-		return;
-	} else {
-		// Load data from stat file
-		fgets(command, sizeof(command), cmdFile);
-	}
-	fclose(cmdFile);
-
-	// Print data
-	printf("%d:", stat_pid);
-
-	if(state) {
-		printf(" %c", stat_state);
-	}
-
-	if(userTime) {
-		printf(" utime=%lu", utime);
-	}
-
-	if(sysTime) {
-		printf(" stime=%lu", stime);
-	}
-
-	if(vMem) {
-		printf(" vmemory=%d", vMemVal);
-	}
-
-	if(comLine) {
-		printf(" [%s]", command);
-	}
-
-	printf("\n");
+void printOne(int pid, bool state, bool userTime, bool sysTime, bool vMem, bool comLine) {
 
 }
